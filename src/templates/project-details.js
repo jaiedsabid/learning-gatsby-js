21b06aada14143103c7320bf5e53e8ca8c4c54e0
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-07-08 14:08:32 +0600

    Project complete

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import {GatsbyImage} from 'gatsby-plugin-image';
+import {GatsbyImage} from "gatsby-plugin-image";
 import * as styles from '../styles/project-details.module.css';
+import {graphql} from "gatsby";
 
-function ProjectDetails() {
+function ProjectDetails({data}) {
+    const {html} = data.markdownRemark;
+    const {title, stack, featuredImg} = data.markdownRemark.frontmatter;
     return (
         <Layout>
             <div className={styles.details}>
-                <h2></h2>
-                <h3></h3>
+                <h2>{title}</h2>
+                <h3>{stack}</h3>
                 <div className={styles.featured}>
-                    {/*<GatsbyImage alt={} image={}/>*/}
+                    <GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData} alt={title}/>
                 </div>
-                    {/*<div className={styles.html} dangerouslySetInnerHTML={}/>*/}
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }}/>
             </div>
         </Layout>
     );
 }
+
+export const data = graphql`
+query ProjectDetails($slug: String) {
+  markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+    html
+    frontmatter {
+      stack
+      title
+      featuredImg {
+        childImageSharp {
+          gatsbyImageData(
+            formats: AUTO
+            pngOptions: {quality: 80}
+            transformOptions: {fit: COVER}
+          )
+        }
+      }
+    }
+  }
+}
+`;
 
 export default ProjectDetails;
